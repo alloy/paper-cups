81cdc4b8c19f451126ee4846532a8770cb0adfef
Eloy Duran <eloy.de.enige@gmail.com>
2009-11-21 15:11:27 +0100

    Only show the first line of the commit message.

--- app/models/service/git_hub.rb
+++ app/models/service/git_hub.rb
@@ -1,13 +1,18 @@
 class Service::GitHub < Service
   def create_message(room, author, params)
-    pushed = JSON.parse(params[:payload])
-    repo   = pushed['repository']['name']
-    branch = pushed['ref'].split('/').last
-    
-    pushed['commits'].each do |commit|
-      committer = commit['author']['name']
-      room.messages.create :author => author, :body =>
-        "[#{repo} - #{branch}] #{commit['message']} (#{commit['url']}) -- #{committer}"
+    @pushed = JSON.parse(params[:payload])
+    @pushed['commits'].each do |commit|
+      room.messages.create :author => author, :body => message(commit)
     end
+  end
+  
+  private
+  
+  def message(commit)
+    @repo   ||= @pushed['repository']['name']
+    @branch ||= @pushed['ref'].split('/').last
+    
+    commit_message = commit['message'].split("\n").first.strip
+    "[#{@repo}/#{@branch}] #{commit_message} (#{commit['url']}) -- #{commit['author']['name']}"
   end
 end
--- test/unit/service/git_hub_test.rb
+++ test/unit/service/git_hub_test.rb
@@ -11,10 +11,10 @@ describe "Service::GitHub" do
     
     message1.author.should == members(:api)
     message1.room.should == room
-    message1.body.should == '[github - master] okay i give in (http://github.com/defunkt/github/commit/41a212ee83ca127e3c8cf465891ab7216a705f59) -- Chris Wanstrath'
+    message1.body.should == '[github/master] okay i give in (http://github.com/defunkt/github/commit/41a212ee83ca127e3c8cf465891ab7216a705f59) -- Chris Wanstrath'
     
     message2.author.should == members(:api)
     message2.room.should == room
-    message2.body.should == '[github - master] update pricing a tad (http://github.com/defunkt/github/commit/de8251ff97ee194a289832576287d6f8ad74e3d0) -- Chris Wanstrath'
+    message2.body.should == '[github/master] update pricing a tad (http://github.com/defunkt/github/commit/de8251ff97ee194a289832576287d6f8ad74e3d0) -- Chris Wanstrath'
   end
 end
