60b671bbeb006b5dd3f9c6cb951b444f160927cf
Eloy Duran <eloy.de.enige@gmail.com>
2011-05-10 20:41:24 +0200

    Refactor a bit.

--- lib/gmail.rb
+++ lib/gmail.rb
@@ -6,27 +6,53 @@ class Gmail
   PORT = 993
   
   def initialize(username, password)
+    connect(username, password)
+  end
+  
+  def emails(conditions)
+    find_ids(conditions).each do |id|
+      yield fetch_email(id)
+      archive_email(id)
+    end
+    disconnect
+  rescue Errno::ENOTCONN, Net::IMAP::NoResponseError, Net::IMAP::ByeResponseError => e
+    Rails.logger.error "[!] Unable to connect to to Gmail: #{e.class.name}"
+  end
+  
+  private
+  
+  def find_ids(conditions)
+    @connection.uid_search(query(conditions))
+  end
+  
+  def fetch_email(id)
+    source = @connection.uid_fetch(id, ['RFC822']).first.attr['RFC822']
+    TMail::Mail.parse(source)
+  end
+  
+  def archive_email(id)
+    @connection.uid_copy(id, "[Gmail]/All Mail")
+    @connection.uid_store(id, "+FLAGS", [:Deleted])
+  end
+  
+  def connect(username, password)
     @connection = Net::IMAP.new(HOST, PORT, true)
     @connection.login(username, password)
     @connection.select('Inbox')
   end
   
-  def emails(conditions)
+  def disconnect
+    @connection.expunge # flush deleted emails
+    @connection.logout
+    @connection.disconnect
+  end
+  
+  def query(conditions)
     query = %w{ NOT DELETED }
     conditions.each do |key, value|
       query << key.to_s.upcase
       query << value
     end
-    @connection.uid_search(query).each do |uid|
-      source = @connection.uid_fetch(uid, ['RFC822']).first.attr['RFC822']
-      yield TMail::Mail.parse(source)
-      @connection.uid_copy(uid, "[Gmail]/All Mail")
-      @connection.uid_store(uid, "+FLAGS", [:Deleted])
-    end
-    @connection.expunge
-    @connection.logout
-    @connection.disconnect
-  rescue Errno::ENOTCONN, Net::IMAP::NoResponseError, Net::IMAP::ByeResponseError => e
-    Rails.logger.error "[!] Unable to connect to to Gmail: #{e.class.name}"
+    query
   end
 end
